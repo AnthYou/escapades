2bc5e0d5b8919860e0c748a684f40a3f13fc7983
Anthony You <you.anthony@yahoo.com>
2020-08-24 18:17:30 +0200

    created Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_161311) do
+ActiveRecord::Schema.define(version: 2020_08_24_161633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 2020_08_24_161311) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["trip_id"], name: "index_applications_on_trip_id"
     t.index ["user_id"], name: "index_applications_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "stars"
+    t.bigint "application_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["application_id"], name: "index_reviews_on_application_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "trips", force: :cascade do |t|
@@ -71,5 +82,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_161311) do
   add_foreign_key "activities", "trips"
   add_foreign_key "applications", "trips"
   add_foreign_key "applications", "users"
+  add_foreign_key "reviews", "applications"
+  add_foreign_key "reviews", "users"
   add_foreign_key "trips", "users"
 end
