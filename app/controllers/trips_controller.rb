a6a8e5c06124f1102dff0717bb943a01a6c23adc
gourouhassiba <gourouhassiba@gmail.com>
2020-08-25 13:02:56 +0200

    add index and show to controller and view

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,15 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title, :description, :destination, :budget_min, :budget_max, :capacity, :return_date, :departure_date, :photo)
+  end
 end
