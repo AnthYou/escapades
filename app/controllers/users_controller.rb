7a6c68fc22c0a1b10b9b86e76b2b565ae881719a
gourouhassiba <gourouhassiba@gmail.com>
2020-08-25 19:16:09 +0200

    add reviews and a profil for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @reviews = Review.all
+    @user_reviews = @reviews.where(user_id: @user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :trips, only: [ :index, :create, :new, :show]
+  resources :users, only: [:show] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
