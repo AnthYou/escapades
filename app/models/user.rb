890e56bfce46102810fc8fd7b78491541ab35d65
Mathieu Valery <mathieuromainvalery@gmail.com>
2020-08-25 14:51:06 +0200

    seed application, posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_one_attached :photo
+  has_many :applications
+  has_many :trips, through: :application
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
