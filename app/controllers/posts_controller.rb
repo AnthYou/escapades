48e54bf48eedbc367c3329df0c98fe73b722383f
Raphael Toubiana <raphael.toubiana@gmail.com>
2020-08-27 23:52:52 +0200

    adding post creation from user/user_id/posts/new route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,28 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     authorize @posts
   end
+
+  def new
+    @post = Post.new
+    @user = current_user
+    authorize @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    authorize @post
+    @post.user_id = current_user.id
+    # @post.save
+    if @post.save!
+      redirect_to user_posts_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :photo, :trip_id)
+  end
 end
-p
