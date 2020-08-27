6ceb1f53c1d5404c033cfbc21c857f62a00583a5
Raphael Toubiana <raphael.toubiana@gmail.com>
2020-08-27 16:38:18 +0200

    creating user_posts index and sample in user profile

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] do
     resources :reviews, only: [ :new, :create, :show ]
-    resources :posts, only: [ :new, :create ]
+    resources :posts, only: [ :new, :create, :index ]
   end
 
   resources :users, only: [:show]
