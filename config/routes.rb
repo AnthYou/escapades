3bcd0effe9816454db5d5f6be34e8d2e2f65f07d
gourouhassiba <gourouhassiba@gmail.com>
2020-08-27 11:20:10 +0200

    correct the usershow view, correct the route to dashboard, add path to profil and dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :reviews, only: [ :new, :create ]
   end
-  resources :users, only: [:show] do
-    member do
-      get :dashboard
-    end
-  end
+  resources :users, only: [:show]
+  get "/dashboard", to: "users#dashboard", as: :dashboard
 end
