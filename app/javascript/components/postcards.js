015788eee5ded267a4613134144771eccc51638a
Raphael Toubiana <raphael.toubiana@gmail.com>
2020-08-31 11:09:02 +0200

    post cards new display

---
+++ app/javascript/components/postcards.js
@@ -0,0 +1,11 @@
+const initPolaroidsRotation = () => {
+  var polaroids = document.querySelectorAll('.polaroid');
+
+  polaroids.forEach(item => {
+    const randomRotation = Math.floor(Math.random() * (6 - -6 + 1) + -6);
+
+    item.style.transform = `rotate(${randomRotation}deg)`
+  })
+}
+
+export {initPolaroidsRotation};
