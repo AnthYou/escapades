b4b5cee467f065b31cadaf671453e694482cf278
Mathieu Valery <mathieuromainvalery@gmail.com>
2020-09-01 16:12:34 +0200

    add transport type in the form

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,10 +1,12 @@
 class Activity < ApplicationRecord
+  TRANSPORT = ['plane', 'car']
   has_one_attached :photo
   geocoded_by :location
   belongs_to :trip
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 30 }
   validates :location, presence: true
+  validates :transport_type, presence: true
 
   after_validation :geocode, if: :will_save_change_to_location?
 end
