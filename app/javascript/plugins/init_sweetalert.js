8a2fbb8dceb0238768b2315f9246d05973beb1fb
Mathieu Valery <mathieuromainvalery@gmail.com>
2020-09-04 12:46:01 +0200

    debug review application

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,44 +1,48 @@
 import swal from 'sweetalert';
 
 const initAcceptSweetalert = (selector, options = {}) => {
-   const swalButton = document.querySelector(".accept-application");
-  if (swalButton) { // protect other pages
-    swalButton.addEventListener('click', () => {
-      swal({
-        title: "Confirm acceptation?",
-        icon: "warning"
-      }).then((value) => {
-        if (value) {
-       
-           swalButton.nextElementSibling.click();
-        }
+    const swalButton = document.querySelectorAll(".accept-application");
+    if (swalButton) { // protect other pages
+      swalButton.forEach((button) => {
+        button.addEventListener('click', () => {
+        swal({
+          title: "Confirm acceptation?",
+          icon: "warning"
+        }).then((value) => {
+          if (value) {
+
+             swalButton.nextElementSibling.click();
+          }
+        });
       });
     });
-  }
-};
+  };
+}
 
 const initRejectSweetalert = (selector, options = {}) => {
-   const swalButton = document.querySelector(".reject-application");
-  if (swalButton) { // protect other pages
-    swalButton.addEventListener('click', () => {
-      swal({
-        title: "Do you really want to reject this application?",
-        text: "This action cannot be reversed",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-      }).then((willDelete) => {
-        if (willDelete) {
-          swal("This application has been rejected!", {
-              icon: "success",
-            }).then((redirect) => {
-              if (redirect) {
-                swalButton.nextElementSibling.click();
-                }
-            });;
-        } else {
-          swal("This application hasn't been rejected");
-        };
+    const swalButton = document.querySelectorAll(".reject-application");
+    if (swalButton) { // protect other pages
+      swalButton.forEach((button) => {
+      button.addEventListener('click', () => {
+        swal({
+          title: "Do you really want to reject this application?",
+          text: "This action cannot be reversed",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        }).then((willDelete) => {
+          if (willDelete) {
+            swal("This application has been rejected!", {
+                icon: "success",
+              }).then((redirect) => {
+                if (redirect) {
+                  swalButton.nextElementSibling.click();
+                  }
+              });;
+          } else {
+            swal("This application hasn't been rejected");
+          };
+        });
       });
     });
   };
